aeeac6a1e211b6de8ac7a304b046ce32cc5a9e31
harsh-julka-bb <harsh-julka-bb@users.noreply.github.com>
2023-12-13 09:03:55 +0000

    jekyll build from Action 28ef14697512c49f0c56aa70bcc270be51625e32

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,133 @@
+var store = [{
+        "title": "Kubernetes Application Developer Certification Tips",
+        "excerpt":"I just passed the Certified Kubernetes Application Developer (CKAD) exam: https://www.cncf.io/certification/ckad/ And I want to share my journey and some tips on how to handle the CKAD preparation and the exam itself. Preparation Starting point First, you need a base, if you have no work experience with production Kubernetes then...","categories": ["DevOps"],
+        "tags": ["Ckad","Kubernetes","Certification","Certified","Kubernetes","K8s"],
+        "url": "/2020/03/04/kubernetes-application-developer-certification-tips",
+        "teaser": "/assets/images/post/kubernetes-banner.png"
+      },{
+        "title": "Building an iOS 14 Widget to Show Account Balance",
+        "excerpt":"Disclaimer: This is merely used as proof of concept rather than a production feature, as there are security points to be taken in consideration for showing private and sensitive data for banking apps and similar applications. WWDC20 has introduced us to iOS 14, which along other features now include widgets...","categories": ["Mobile"],
+        "tags": ["Mobile","iOS14","Swift","Widget"],
+        "url": "/2020/07/24/building-an-ios14-widget-to-show-account-balance",
+        "teaser": "/assets/images/post/ios-widget-banner.png"
+      },{
+        "title": "Configuration Driven UI from Epoxy to Compose",
+        "excerpt":"This is a story of how we came up with the solution for Configuration driven UI (a level below server-driven UI) in Android at Backbase. It’s not a super perfect system or not highly reactive as Jetpack compose, but it absolutely solves the use-case: to create a framework or library,...","categories": ["Mobile"],
+        "tags": ["Mobile","Android","Jetpack","Epoxy"],
+        "url": "/2020/08/14/android-configuration-driven-ui-from-epoxy-to-compose",
+        "teaser": "/assets/images/post/epoxy-compose-banner.png"
+      },{
+        "title": "Flexible configuration with Deferred Resources",
+        "excerpt":"Our feature libraries, which include UI, have a number of varying requirements regarding configuration by our customers. (My colleague Hari wrote about one such library’s requirements here.) Resources (text, colors, images, etc.) are a common type of configuration, and can be defined in various ways—typically in code, resources, or theme...","categories": ["Mobile"],
+        "tags": ["Mobile","Android","Open-source","Deferred-Resources"],
+        "url": "/2021/06/16/android-flexible-configuration-with-deferred-resources",
+        "teaser": "/assets/images/post/deferred-resources-banner.png"
+      },{
+        "title": "Hello, Blockchain!",
+        "excerpt":"This tutorial walks you through the process of creating a very minimal “Hello, world!” - style React app that reads and displays data from the Joystream blockchain. Along the way, I comment a thing or two about scaffolding React applications, JSON-RPC APIs and about the blockhain itself. Blockchain? Really? It...","categories": ["Dev"],
+        "tags": ["Blockchain","React"],
+        "url": "/2021/09/10/intro-to-blockchain-react",
+        "teaser": "/assets/images/post/blockchain_banner.png"
+      },{
+        "title": "The ultimate guide to Slack etiquette",
+        "excerpt":"We all know that COVID changed the world we live in today. It also changed the way most people work. Although remote work is nothing new for a lot of people, many of us had to adapt to it. Before, we could simply have a coffee with our team mates...","categories": ["Remote Work"],
+        "tags": ["Slack","Remote","Work","Etiquette"],
+        "url": "/2021/12/01/the-ultimate-guide-to-slack-etiquette",
+        "teaser": "/assets/images/post/slack-etiquette-banner.png"
+      },{
+        "title": "Deploying an Angular app on Azure",
+        "excerpt":"Introduction Backbase has been working towards a web architecture that follows Jamstack principles and aligns with Angular best practices. Our goal is to ensure our web app’s rendering strategy is decoupled from the backend and hosted on the best infrastructure possible. As a first step in that direction, we replaced...","categories": ["Frontend"],
+        "tags": ["Frontend","Angular","Azure","i18n"],
+        "url": "/2022/06/08/deploying-angular-app-on-azure",
+        "teaser": "/assets/images/post/angular-azure-banner.png"
+      },{
+        "title": "Ease your life at work with Fastlane",
+        "excerpt":"As a software Tester, are you curious in finding the solutions to the below questions? (for iOS) How to reduce test execution time by running in parallel on physical devices and simulators? How to generate test reports with screenshots and run tests via a command-line interface? How development team can...","categories": ["Software Quality Assurance"],
+        "tags": ["Mobile","iOS","testing"],
+        "url": "/2022/07/01/ios-mobile-testing-ease-your-life-at-work-with-fastlane",
+        "teaser": "/assets/images/post/fastlane_banner.png"
+      },{
+        "title": "Android Mobile Testing: Fragments",
+        "excerpt":"What is Fragment testing on Android Firstly, we need to know what Fragments in Android are. Reusable containers Fragments are reusable containers within our app allowing us to present the same user interface layout in a variety of activities and layout configurations. Given the versatility of fragments, it’s important to...","categories": ["Software Quality Assurance"],
+        "tags": ["Mobile","Android","testing"],
+        "url": "/2022/11/16/android-fragment-testing",
+        "teaser": "/assets/images/post/espresso-banner.png"
+      },{
+        "title": "Shift Left Approach to Testing",
+        "excerpt":"“Shift left” testing movement is about pushing testing towards the early stages of Software Development Life Cycle. By testing early and often, a project can reduce the number of bugs and increase the quality of the code. Traditional Quality Model vs Shift Left Model Up until the late 1990s, stakeholders...","categories": ["Software Quality Assurance"],
+        "tags": ["Testing,","Quality","Assurance"],
+        "url": "/2023/05/01/shift-left-approach-to-testing",
+        "teaser": "/assets/images/post/shift-left-banner.png"
+      },{
+        "title": "NgOptimizedImage Directive in Angular 15",
+        "excerpt":"Introduction Images constitute a major part of any web application. They heavily impact the page loading times and performance of the web page. They are one of the main contributors to the Core Web Vitals, identified by Google, to measure website performance. The metrics that make up the core web...","categories": ["Frontend"],
+        "tags": ["Web","NgOptimizedImage","Angular"],
+        "url": "/2023/05/12/ng-optimized-image-directive-in-angular-15",
+        "teaser": "/assets/images/post/ng-optimized-image-directive-in-angular-banner.png"
+      },{
+        "title": "Hacking Netflix Eureka",
+        "excerpt":"Eureka is a service registry used to make micro-services to find each other. What can go wrong if the registry server becomes available from the internet? What if a malicious user registers his new micro-service? It looks like the registry can become a point of failure from a security point...","categories": ["Security"],
+        "tags": ["Eureka","Security"],
+        "url": "/2023/05/16/hacking-netflix-eureka",
+        "teaser": "/assets/images/post/hacking-netflix-eureka/hacking-netflix-eureka.png"
+      },{
+        "title": "Angular standalone components",
+        "excerpt":"Introduction Until now, developers have been using NgModules to specify the components, directives, pipes, etc that are available for use in templates, however, after the release of Angular standalone components, you don’t need to do this anymore in the NgModules. Angular standalone feature was fully released with Angular v15. Angular...","categories": ["Frontend"],
+        "tags": ["Web","Angular","standalone"],
+        "url": "/2023/07/03/angular-standalone-components",
+        "teaser": "/assets/images/post/angular-standalone-components.png"
+      },{
+        "title": "The Better Way with Ruby Gems",
+        "excerpt":"Introduction Working with an iOS project involves the usage of tools such as Cocoapods, Slather, SwiftLint, Fastlane and more. Ideally, all engineers working on a particular codebase - as well as the CI machine it runs on - should maintain the same versions of these tools. Some of these tools...","categories": ["mobile"],
+        "tags": ["Mobile","Android","iOS","Ruby"],
+        "url": "/2023/09/29/the-better-way-ruby-gems",
+        "teaser": "/assets/images/post/ruby-gems/the_better_way_ruby_gems.png"
+      },{
+        "title": "Installing HMS Core in Android Studio Emulator",
+        "excerpt":"Introduction This is a quick guide with few and simple instructions on installing HMS Core in Android Studio Emulator. If you want to test your HMS implementation and you do not have a Huawei device, this is your best option. Steps 1. Create a new device with only Google APIs,...","categories": ["Android"],
+        "tags": ["Mobile","Android","Huawei","HMS"],
+        "url": "/2023/09/30/installing-hms-core-in-as-emulator",
+        "teaser": "/assets/images/post/hms-core/hms_core_banner.png"
+      },{
+        "title": "Code Coverage for Unit Tests",
+        "excerpt":"Introduction Code coverage is the percentage of lines of your code that is being executed by your tests. This is an indicative of how safe your code is and how bug proof it can be while refactoring or adding new features. Code coverage helps in the easy maintenance of the...","categories": ["QA"],
+        "tags": ["QA","Mobile","Android","iOS","Tests"],
+        "url": "/2023/10/06/code-coverage",
+        "teaser": "/assets/images/post/code-coverage/code_coverage.png"
+      },{
+        "title": "Angular Unit Testing with Spectator",
+        "excerpt":"Introduction Spectator is a powerful testing tool designed to simplify Angular testing. It is built on top of the TestBed API and effectively reduces the boilerplate code associated with the built-in TestBed API. By doing so, Spectator delivers a more efficient and developer-friendly testing experience. In this article, we discuss...","categories": ["Frontend"],
+        "tags": ["Web","Angular","testing"],
+        "url": "/2023/10/25/angular-unit-testing-with-spectator",
+        "teaser": "/assets/images/post/spectator-banner.png"
+      },{
+        "title": "Visual Testing using Playwright",
+        "excerpt":"“Visual testing” is a software testing technique that evaluates the visible output of an app and compares that output against the results expected by a known-good source, such as a design mockup created in Photoshop or Figma, or a screenshot from a previous version of the same app. Visual testing...","categories": ["Software Quality Assurance"],
+        "tags": ["Testing,","Quality","Assurance"],
+        "url": "/2023/11/03/visual-testing-using-playwright",
+        "teaser": "/assets/images/post/visual-testing-banner.png"
+      },{
+        "title": "A New Chapter: Exploring Azure Resources",
+        "excerpt":"Demystifying Cloud Computing: A Quick Introduction Introduction to Cloud Computing Cloud computing is a transformative technology that delivers various services over the internet. Instead of relying on local servers or computers, hosts these services remotely in data centers, commonly referred to as “the cloud.” This model offers benefits like scalability,...","categories": ["System Engineering"],
+        "tags": ["DevOps,","System","Engineering"],
+        "url": "/2023/11/21/a-new-chapter-exploring-azure-resources",
+        "teaser": "/assets/images/post/azure-banner.png"
+      },{
+        "title": "Angular & Micro Frontends",
+        "excerpt":"Introduction Micro Frontends is a modern architectural approach for developing web applications. Instead of creating a large, monolithic web application, it involves building multiple smaller frontend applications. Each of these small application has a specific feature, which can be independently deployed and scaled. These micro frontends interact with each other...","categories": ["Frontend"],
+        "tags": ["Micro","Frontends"],
+        "url": "/2023/12/13/setup-microfrontend",
+        "teaser": "/assets/images/post/angular_microfrontends.png"
+      },{
+        "title": "Software Engineering @ Backbase",
+        "excerpt":"A short introduction What are these principles and why we need them? Backbase people are craftspeople: we understand the importance of quality and we believe in great design. As an engineer, think of when you are happy with the standard of your work. What makes you feel proud of your...","categories": ["Engineering"],
+        "tags": [],
+        "url": "/principles/software-engineering",
+        "teaser": "/assets/images/principles/software-engineering/cover.png"
+      },{
+        "title": "InnerSource @ Backbase",
+        "excerpt":"What is InnerSource? InnerSource is the use of open source best practices and establishment of an open source culture — within an organisation like Backbase. You may develop proprietary software, while opening up work between developers and teams. With the increasing pace of technological change, internal communication and collaboration have...","categories": [],
+        "tags": [],
+        "url": "/principles/innersource",
+        "teaser": "/assets/images/innersource.png"
+      }]
