1764ef038c9863865d888280f750958bfc207b70
Aleksei Morozov <a1exy.moroz@gmail.com>
2024-02-08 16:27:58 +0100

    highlight current section on right sidebar of post

--- src/app/components/table-of-content/table-of-content.component.ts
+++ src/app/components/table-of-content/table-of-content.component.ts
@@ -1,5 +1,5 @@
 import { FlatTreeControl } from '@angular/cdk/tree';
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Inject, Input } from '@angular/core';
 import {
   MatTreeFlatDataSource,
   MatTreeFlattener,
@@ -9,7 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderNode, HeaderTreeNode } from '../../core/model/content.model';
 import { RouterLink } from '@angular/router';
-import { NgClass } from '@angular/common';
+import { DOCUMENT, NgClass } from '@angular/common';
 
 @Component({
   selector: 'blog-table-of-content',
@@ -22,8 +22,53 @@ export class TableOfContentComponent {
   @Input() set headers(headers: HeaderNode[]) {
     this.dataSource.data = headers;
     this.treeControl.expandAll();
+    if (headers.length) {
+      this.createOffsets(headers);
+      this.activeHeader = this.offsetHeaders[0];
+    }
   }
+
   @Input() current!: string;
+
+  offsetHeaders: string[] = [];
+
+  activeHeader: string | null = null;
+
+  @HostListener('document:scroll', ['$event'])
+  public onViewportScroll() {
+    if (this.offsetHeaders.length) {
+      const scroll = Math.ceil(this.document.documentElement.scrollTop);
+      const fullHeightWithScroll = this.document.body.scrollHeight;
+      const fullHeight =
+        this.document.body.scrollHeight - this.document.body.clientHeight;
+
+      for (let i = 0; i < this.offsetHeaders.length; i++) {
+        const element = this.offsetHeaders[i];
+        const nextElement = this.offsetHeaders[i + 1];
+
+        const offset = this.document.getElementById(element)?.offsetTop;
+        const nextOffset = nextElement
+          ? this.document.getElementById(nextElement)?.offsetTop
+          : fullHeightWithScroll;
+
+        if (offset == null || nextOffset == null) {
+          continue;
+        }
+
+        if (fullHeight === scroll) {
+          this.activeHeader = this.offsetHeaders[this.offsetHeaders.length - 1];
+          return;
+        }
+
+        if (scroll >= offset && scroll < nextOffset) {
+          this.activeHeader = element;
+          return;
+        }
+      }
+    }
+  }
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
   private _transformer = (node: HeaderNode, level: number) => {
     return {
@@ -49,4 +94,14 @@ export class TableOfContentComponent {
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
   hasChild = (_: number, node: HeaderTreeNode) => node.expandable;
+
+  private createOffsets(data: HeaderNode[], array = this.offsetHeaders) {
+    for (const element of data) {
+      array.push(element.id);
+
+      if (element.children.length) {
+        this.createOffsets(element.children, array);
+      }
+    }
+  }
 }
