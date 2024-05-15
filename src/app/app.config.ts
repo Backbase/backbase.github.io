ac256d17505a7af782ab852130e51482e30b993f
Robinson Zimmermann <16561945+robinsonzimmermann@users.noreply.github.com>
2024-05-15 11:21:20 +0200

    Feature/implement o11y (#119)
    * Integrate app with OpenTelemetry

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,9 +14,11 @@ import { MarkdownModule, MarkdownService } from 'ngx-markdown';
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import markdownConfig from './markdown.config';
 import { DOCUMENT } from '@angular/common';
+import { AUTHORS_AVATAR_PATH_TOKEN, USE_PROCESSED_IMAGES, O11Y_CONFIG_TOKEN } from './core/config/configuration-tokens';
 import { HtmlInMarkdownService } from './core/services/html-in-markdown.service';
+import { ObservabilityConfig } from './core/model/observability.model';
+import * as pkg from '../../package.json';
 import { AssetsService } from './core/services/assets.service';
-import { AUTHORS_AVATAR_PATH_TOKEN, USE_PROCESSED_IMAGES } from './core/config/configuration-tokens';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -37,8 +39,19 @@ export const appConfig: ApplicationConfig = {
       })
     ),
     {
+      provide: O11Y_CONFIG_TOKEN,
+      useValue: <ObservabilityConfig>{
+        apiKey: '68435ee4-f575-4dc0-968b-c43665373f5c',
+        appName: 'bb-engineering',
+        version: pkg.version,
+        url: 'https://rum-collector.backbase.io/v1/traces',
+        enabled: !isDevMode(),
+      },
+    },
+    {
       provide: APP_INITIALIZER,
-      useFactory: markdownConfig,
+      multi: true,
+      useFactory: (...deps: any) => () => markdownConfig.apply(this, deps),
       deps: [MarkdownService, DOCUMENT, HtmlInMarkdownService, AssetsService],
     },
     {
