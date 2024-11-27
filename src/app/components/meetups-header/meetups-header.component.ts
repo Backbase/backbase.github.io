fa889393c9fd563c8add734077862379c237a601
Dawid Ryczko <dawidry@backbase.com>
2024-11-27 09:43:06 +0100

    Added meetup header

--- src/app/components/meetups-header/meetups-header.component.ts
+++ src/app/components/meetups-header/meetups-header.component.ts
@@ -4,9 +4,8 @@ import { MatChip } from '@angular/material/chips';
 import { PostImageComponent } from '../post-image/post-image.component';
 import { PostUrlPipe } from '../../core/utils/post-url.pipe';
 import { RouterLink, RouterModule } from '@angular/router';
-import { ButtonComponent } from '../button/button.component';
 import { MatAnchor, MatButtonModule } from '@angular/material/button';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -17,14 +16,11 @@ import { CommonModule } from '@angular/common';
     PostImageComponent,
     PostUrlPipe,
     RouterLink,
-    ButtonComponent,
     MatAnchor,
-    MatIcon,
     CommonModule,
     MatButtonModule,
     MatIconModule,
     RouterModule,
-    ButtonComponent,
   ],
   templateUrl: './meetups-header.component.html',
   styleUrl: './meetups-header.component.scss',
