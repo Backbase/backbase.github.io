9abc20abbc06a24b7ef7526096afaadb20e13ee7
Dawid Ryczko <dawidry@backbase.com>
2024-11-27 09:40:02 +0100

    Added meetup header

--- src/app/components/meetups-header/meetups-header.component.ts
+++ src/app/components/meetups-header/meetups-header.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from '../../core/model/post.model';
+import { MatChip } from '@angular/material/chips';
+import { PostImageComponent } from '../post-image/post-image.component';
+import { PostUrlPipe } from '../../core/utils/post-url.pipe';
+import { RouterLink, RouterModule } from '@angular/router';
+import { ButtonComponent } from '../button/button.component';
+import { MatAnchor, MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'blog-meetups-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatChip,
+    PostImageComponent,
+    PostUrlPipe,
+    RouterLink,
+    ButtonComponent,
+    MatAnchor,
+    MatIcon,
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+    ButtonComponent,
+  ],
   templateUrl: './meetups-header.component.html',
   styleUrl: './meetups-header.component.scss',
 })
-export class MeetupsHeaderComponent {}
+export class MeetupsHeaderComponent {
+  @Input() post!: Post;
+}
